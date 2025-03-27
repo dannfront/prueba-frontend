a93b28b8043c5d42587d9584e4282add25164591
dannfront <santi-dy240@hotmail.com>
2025-03-27 11:23:33 -0600

    add redirect

--- src/components/shared/ImageUser.tsx
+++ src/components/shared/ImageUser.tsx
@@ -4,7 +4,7 @@ export default function ImageUser() {
     return (
         <div className="bg-yellowPrimary rounded-full size-[50px] lg:size-[60px] content-center">
 
-            <img className="size-[42px]  lg:size-[52px] rounded-full mx-auto" src="/src/assets/images/user.png" alt="photo-user" />
+            <img className="size-[42px]  lg:size-[52px] rounded-full mx-auto" src="/images/user.png" alt="photo-user" />
         </div>
     )
 }
